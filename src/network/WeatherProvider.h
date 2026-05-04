b7d9c59933b72cd09143efea87a3d8ee61a32660
unknown <samandartoshtemirovmaratovich@example.com>
2026-05-04 17:13:14 +0500

    refactor(code): simplify Q_PROPERTY system using MEMBER keyword

--- src/network/WeatherProvider.h
+++ src/network/WeatherProvider.h
@@ -21,6 +21,13 @@ public:
     QString condition;
     double temperature;
     int humidity;
+
+    bool operator==(const HourlyForecastUnit& other) const {
+        return time == other.time &&
+            condition == other.condition &&
+            qFuzzyCompare(temperature, other.temperature) &&
+            humidity == other.humidity;
+    }
 };
 
 struct DailyForecastUnit final {
@@ -39,6 +46,14 @@ public:
     double minTemperature;
     QString description;
 
+    bool operator==(const DailyForecastUnit& other) const {
+        return week == other.week &&
+            condition == other.condition &&
+            qFuzzyCompare(maxTemperature, other.maxTemperature) &&
+            qFuzzyCompare(minTemperature, other.minTemperature) &&
+            description == other.description;
+    }
+
 private:
     QString date;
 
@@ -52,58 +67,33 @@ class WeatherProvider final : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY(double temperature READ getTemperature NOTIFY weatherDataChanged)
-    Q_PROPERTY(double maxTemperature READ getMaxTemperature NOTIFY forecastDataChanged)
-    Q_PROPERTY(double minTemperature READ getMinTemperature NOTIFY forecastDataChanged)
-    Q_PROPERTY(double feelsLike READ getFeelsLike NOTIFY weatherDataChanged)
-    Q_PROPERTY(QString condition READ getCondition NOTIFY weatherDataChanged)
-    Q_PROPERTY(QString description READ getDescription NOTIFY weatherDataChanged)
-    Q_PROPERTY(int humidity READ getHumidity NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(double windSpeed READ getWindSpeed NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(QString windDirection READ getWindDirection NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(int pressure READ getPressure NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(int cloudiness READ getCloudiness NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(int visibility READ getVisibility NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(double dewPoint READ getDewPoint NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(int precipitation READ getPrecipitation NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(int uvIndex READ getUVIndex NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(QString uvLevel READ getUVLevel NOTIFY weatherDetailsDataChanged)
-    Q_PROPERTY(QString sunrise READ getSunrise NOTIFY weatherDataChanged)
-    Q_PROPERTY(QString sunset READ getSunset NOTIFY weatherDataChanged)
-    Q_PROPERTY(HourlyForecastModel hourlyForecastModel READ getHourlyForecastModel NOTIFY forecastDataChanged)
-    Q_PROPERTY(DailyForecastModel dailyForecastModel READ getDailyForecastModel NOTIFY forecastDataChanged)
-    Q_PROPERTY(QString lastFetchedTime READ getLastFetchedTime NOTIFY weatherDataChanged)
-    Q_PROPERTY(bool weatherDataLoading READ getWeatherDataLoading NOTIFY weatherDataLoadingChanged)
-    Q_PROPERTY(bool weatherDetailsDataLoading READ getWeatherDetailsDataLoading NOTIFY weatherDetailsDataLoadingChanged)
-    Q_PROPERTY(bool forecastDataLoading READ getForecastDataLoading NOTIFY forecastDataLoadingChanged)
+    Q_PROPERTY(double temperature MEMBER temperature NOTIFY weatherDataChanged)
+    Q_PROPERTY(double maxTemperature MEMBER maxTemperature NOTIFY forecastDataChanged)
+    Q_PROPERTY(double minTemperature MEMBER minTemperature NOTIFY forecastDataChanged)
+    Q_PROPERTY(double feelsLike MEMBER feelsLike NOTIFY weatherDataChanged)
+    Q_PROPERTY(QString condition MEMBER condition NOTIFY weatherDataChanged)
+    Q_PROPERTY(QString description MEMBER description NOTIFY weatherDataChanged)
+    Q_PROPERTY(int humidity MEMBER humidity NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(double windSpeed MEMBER windSpeed NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(QString windDirection MEMBER windDirection NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(int pressure MEMBER pressure NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(int cloudiness MEMBER cloudiness NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(int visibility MEMBER visibility NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(double dewPoint MEMBER dewPoint NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(int precipitation MEMBER precipitation NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(int uvIndex MEMBER uvIndex NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(QString uvLevel MEMBER uvLevel NOTIFY weatherDetailsDataChanged)
+    Q_PROPERTY(QString sunrise MEMBER sunrise NOTIFY weatherDataChanged)
+    Q_PROPERTY(QString sunset MEMBER sunset NOTIFY weatherDataChanged)
+    Q_PROPERTY(HourlyForecastModel hourlyForecastModel MEMBER hourlyForecastModel NOTIFY forecastDataChanged)
+    Q_PROPERTY(DailyForecastModel dailyForecastModel MEMBER dailyForecastModel NOTIFY forecastDataChanged)
+    Q_PROPERTY(QString lastFetchedTime MEMBER lastFetchedTime NOTIFY weatherDataChanged)
+    Q_PROPERTY(bool weatherDataLoading MEMBER weatherDataLoading NOTIFY weatherDataLoadingChanged)
+    Q_PROPERTY(bool weatherDetailsDataLoading MEMBER weatherDetailsDataLoading NOTIFY weatherDetailsDataLoadingChanged)
+    Q_PROPERTY(bool forecastDataLoading MEMBER forecastDataLoading NOTIFY forecastDataLoadingChanged)
 
 public:
     explicit WeatherProvider(QObject *parent = nullptr);
-
-    double getTemperature() const;
-    double getMaxTemperature() const;
-    double getMinTemperature() const;
-    double getFeelsLike() const;
-    int getHumidity() const;
-    double getWindSpeed() const;
-    QString getWindDirection() const;
-    int getPressure() const;
-    int getCloudiness() const;
-    double getVisibility() const;
-    double getDewPoint() const;
-    double getPrecipitation() const;
-    QString getUVLevel() const;
-    int getUVIndex() const;
-    QString getCondition() const;
-    QString getDescription() const;
-    QString getSunrise() const;
-    QString getSunset() const;
-    HourlyForecastModel getHourlyForecastModel() const;
-    DailyForecastModel getDailyForecastModel() const;
-    QString getLastFetchedTime() const;
-    bool getWeatherDataLoading() const;
-    bool getWeatherDetailsDataLoading() const;
-    bool getForecastDataLoading() const;
 
     Q_INVOKABLE void fetchWeather(const QString &city);
     Q_INVOKABLE void updateWeather();
